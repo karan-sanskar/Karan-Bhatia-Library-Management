b6cced9243ebd3fb033f3c9b0c5232a336bce36a
Karan Bhatia <karanbhatia.kb123@gmail.com>
2024-07-30 12:50:48 +0530

    Library management system done

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, Karan Bhatia and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -9,3 +9,17 @@ class LibraryMember(Document):
 	# this method will run every time a document is saved
     def before_save(self):
         self.full_name = f'{self.first_name} {self.last_name or ""}'
+
+    # check before submitting this document
+    def before_submit(self):
+        exists = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                # check if the membership's end date is later than this membership's start date
+                "to_date": (">", self.from_date),
+            },
+        )
+        if exists:
+            frappe.throw("There is an active membership for this member")
